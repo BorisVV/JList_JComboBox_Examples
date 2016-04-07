19fdc2e1db5483ad39c46b3bfa145ff3a3b0ca40
clara <clara@clara.com>
2016-04-07 14:25:43 -0500

    JList and JComboBox examples

--- src/com/creditcard/CCValidator.java
+++ src/com/creditcard/CCValidator.java
@@ -1,42 +1,73 @@
 package com.creditcard;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.regex.Pattern;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-/**
- * Created by admin on 4/7/16.
- */
+
 public class CCValidator extends JFrame {
     private JPanel rootPanel;
     private JTextField creditCardNumbertextField;
     private JButton validateButton;
     private JButton quitButton;
     private JLabel validMessageLabel;
+    private JComboBox ccTypeComboBox;
 
+    private boolean resetMessageOnKeyPress = false;
 
-    protected CCValidator(){
+    private final String VISA = "Visa";
+    private final String MASTERCARD = "Mastercard";
+    private final String AMEX = "American Express";
+
+    protected CCValidator() {
 
         super("Credit Card Validator");
         setContentPane(rootPanel);
+
+        setPreferredSize(new Dimension(500, 200));   //Set preferred size before call to pack()
+
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        ccTypeComboBox.addItem(VISA);
+        ccTypeComboBox.addItem(MASTERCARD);
+        ccTypeComboBox.addItem(AMEX);
+
 
         validateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 String ccNumber = creditCardNumbertextField.getText();
-                if (isVisaCreditCardNumberValid(ccNumber)) {
+
+                String cardType = ccTypeComboBox.getSelectedItem().toString();
+
+                boolean valid = false;
+
+                if (cardType.equals(VISA)) {
+                    valid = isVisaCreditCardNumberValid(ccNumber);
+                }
+                else if (cardType.equals(MASTERCARD)) {
+                    //TODO Mastercard validation
+                }
+                else if (cardType.equals(AMEX)) {
+                    //TODO American express card validation
+                }
+
+                if (valid) {
                     validMessageLabel.setText("Credit card number is valid");
                 } else {
                     validMessageLabel.setText("Credit card number is invalid");
                 }
 
+                resetMessageOnKeyPress = true;
             }
         });
+
 
         quitButton.addActionListener(new ActionListener() {
             @Override
@@ -46,12 +77,31 @@ public class CCValidator extends JFrame {
             }
         });
 
+        // Listener for user making selections from JComboBox
+        ccTypeComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                validateButton.setText("Click to validate " + ccTypeComboBox.getSelectedItem());
+            }
+        });
 
+        // Listener for user typing in JTextField.
+        // Used to reset the valid or not valid message when user enters a new card.
+        creditCardNumbertextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                if (resetMessageOnKeyPress) {
+                    validMessageLabel.setText("~ Valid or not valid displayed here ~");
+                    resetMessageOnKeyPress = false;
+                }
+            }
+        });
     }
+
 
     private static boolean isVisaCreditCardNumberValid(String cc) {
         if (!cc.startsWith("4") || (cc.length() != 16)){
-            System.out.println("Doesnt start with 4 or length wrong");
             return false;
         }
         int sum = 0;
